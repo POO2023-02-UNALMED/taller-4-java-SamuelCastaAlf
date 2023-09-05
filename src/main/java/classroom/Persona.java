43044533f6105debafc9523b8e61a1f6a5e4d3fb
SamuelCastaAlf <92555846+SamuelCastaAlf@users.noreply.github.com>
2023-09-04 21:08:31 -0500

    primer subida, falta asignatura
    subida de algunas clases

--- src/main/java/classroom/Persona.java
+++ src/main/java/classroom/Persona.java
@@ -2,27 +2,30 @@ package classroom;
 
 public class Persona {
 
-    final long cedula;
+    long cedula;
     String nombre;
     static int totalPersonas;
     
     static {
         totalPersonas = 0;
-        cedula = 3;
     }
 
+    public Persona() {
+        this.nombre = nombre;
+        totalPersonas++;
+    }
+        
+    public Persona(String nombre, long cedula) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        totalPersonas++;
+    }
     public Persona(long cedula, String nombre) {
         this.cedula = cedula;
         this.nombre = nombre;
         totalPersonas++;
     }
     
-    public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
-        this.nombre = nombre;
-        totalPersonas++;
-    }
-
     public Persona(long cedula) {
         this.cedula = cedula;
         this.nombre = "";
