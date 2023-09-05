5766635c89851cfff0401e69afa22260b0988e6d
SamuelCastaAlf <92555846+SamuelCastaAlf@users.noreply.github.com>
2023-09-04 21:16:56 -0500

    subida asignatura y prueba
    prueba

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -10,15 +10,14 @@ public class Asignatura {
 
     public Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
-        this(0);
     }
 
-    public Asignatura(int codigoInterno,Tipo tipo) {
+    public Asignatura(int codigoInterno, Tipo tipo) {
         this("Sin nombre", codigoInterno, 0,tipo);
     }
 
-    public Asignatura(int codigoExterno,Tipo tipo) {
-        this("Sin nombre", 0, codigoExterno,tipo);
+    public Asignatura(Tipo tipo, int codigoExterno) {
+        this("Sin nombre",codigoExterno, 0,tipo);
     }
 
     public Asignatura(String nombre, Tipo tipo) {
@@ -43,8 +42,8 @@ public class Asignatura {
         this.codigoInterno = codigoInterno;
     }
 
-    public void cambiarDatos(int codigoExterno) {
-        this.codigoExterno = codigoExterno;
+    public void cambiarDatos(double codigoExterno) {
+        this.codigoExterno = (int)codigoExterno;
     }
 
     public void cambiarDatos(String nombre) {
